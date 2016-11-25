e1a056d0e5e604286cb18fc0b6f20807bbce0369
meawoppl <meawoppl@gmail.com>
2016-11-24 21:21:38 -0800

    WIP

--- pypenrose/line.py
+++ pypenrose/line.py
@@ -1,19 +1,36 @@
 import numpy as np
 
 
+class Colinear(RuntimeError):
+    pass
+
+
 class Line:
     def __init__(self, x, y, i):
+        assert np.all(np.isfinite((x, y, i))), "Not valid value for line"
         self.x = x
         self.y = y
         self.i = i
 
-    def add_to_plot():
+    def add_to_plot(plt):
+        # Determine the plot extents
+        # Determing where to draw lines within those extents
+        # Draw the lines
         pass
 
     def intercept(self, othr):
         mat = np.matrix(
             [[self.x, self.y],
              [othr.x, othr.y]])
-        vec = np.matrix((self.i, othr.i))
+        vec = np.matrix([self.i, othr.i]).T
 
-        return mat.I * vec
+        try:
+            inv = mat.I
+        except np.linalg.linalg.LinAlgError as e:
+            raise Colinear() from e
+
+        # Gross cohersion to tuple(x, y)
+        return tuple(np.array((inv * vec)).flatten().tolist())
+
+    def __str__(self):
+        return "Line: %0.2fx + %0.2fy = %0.2f" % (self.x, self.y, self.i)
--- pypenrose/line_tests.py
+++ pypenrose/line_tests.py
@@ -1,2 +1,48 @@
-import line
+import nose.tools
+
+import pypenrose.line as line
+
+
 def test_line_init():
+    line.Line(1, 2, 3)
+    line.Line(0, 2, 3)
+    line.Line(1, 0, 3)
+
+    # MRG: :/
+    line.Line(0, 0, 0)
+
+
+def test_line_init_invalid():
+    with nose.tools.assert_raises(AssertionError):
+        line.Line(float("inf"), 0, 0)
+
+    with nose.tools.assert_raises(AssertionError):
+        line.Line(0, float("nan"), 0)
+
+
+def test_line_intercept():
+    line1 = line.Line(0, 1, 1)
+    line2 = line.Line(1, 0, 1)
+
+    i1 = line1.intercept(line2)
+    nose.tools.assert_equal(i1, (1, 1))
+
+
+def test_line_intersect():
+    line1 = line.Line(0, 1, 1)
+
+    with nose.tools.assert_raises(line.Colinear):
+        line1.intercept(line1)
+
+    line3 = line.Line(1, -1, 1)
+    line4 = line.Line(-1, 1, -1)
+    with nose.tools.assert_raises(line.Colinear):
+        line3.intercept(line4)
+
+    with nose.tools.assert_raises(line.Colinear):
+        line4.intercept(line3)
+
+
+def test_line_string():
+    line1 = line.Line(1, 1, 1)
+    assert str(line1) == "Line: 1.00x + 1.00y = 1.00"
