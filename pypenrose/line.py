47af31c0ad68434dbe2817811f4201c1af6d5dc0
meawoppl <meawoppl@gmail.com>
2016-11-25 23:56:20 -0800

    Added needed methods and tests

--- pypenrose/line.py
+++ pypenrose/line.py
@@ -8,17 +8,46 @@ class Colinear(RuntimeError):
 class Line:
     def __init__(self, x, y, i):
         assert np.all(np.isfinite((x, y, i))), "Not valid value for line"
+        assert (x != 0) or (y != 0), "Not a valid equation"
         self.x = x
         self.y = y
         self.i = i
 
-    def add_to_plot(plt):
+    @classmethod
+    def from_two_points(cls, px1, py1, px2, py2):
+        m = (py2 - py1) / (px2 - px1)
+        return cls.from_point_slope(px1, py1, m)
+
+    @classmethod
+    def from_point_slope(cls, px, py, m):
+        if m == float("inf"):
+            return cls(1, 0, px)
+
+        # (y-py) = m (x - px)
+        # -m (x)  + y = - m (px) + py
+        return cls(-m, 1, (-m * px) + py)
+
+    def __eq__(self, other):
+        try:
+            self.intersect(other)
+        except Colinear:
+            return True
+        return False
+
+    def y_at(self, x_v):
+        return Line(1, 0, x_v).intersect(self)[1]
+
+    def x_at(self, y_v):
+        return Line(0, 1, y_v).intersect(self)[0]
+
+    def add_to_plot(plt, extents=(-10, 10, -10, 10)):
         # Determine the plot extents
         # Determing where to draw lines within those extents
         # Draw the lines
         pass
 
-    def intercept(self, othr):
+    def intersect(self, othr):
+        assert isinstance(othr, Line)
         mat = np.matrix(
             [[self.x, self.y],
              [othr.x, othr.y]])
--- pypenrose/line_tests.py
+++ pypenrose/line_tests.py
@@ -1,48 +1,82 @@
 import nose.tools
 
-import pypenrose.line as line
+from pypenrose.line import Line, Colinear
 
 
 def test_line_init():
-    line.Line(1, 2, 3)
-    line.Line(0, 2, 3)
-    line.Line(1, 0, 3)
+    Line(1, 2, 3)
+    Line(0, 2, 3)
+    Line(1, 0, 3)
 
-    # MRG: :/
-    line.Line(0, 0, 0)
+    with nose.tools.assert_raises(AssertionError):
+        Line(0, 0, 0)
+
+    with nose.tools.assert_raises(AssertionError):
+        Line(0, 0, 45)
+
+    with nose.tools.assert_raises(AssertionError):
+        Line(float("inf"), 1, 3)
 
 
 def test_line_init_invalid():
     with nose.tools.assert_raises(AssertionError):
-        line.Line(float("inf"), 0, 0)
+        Line(float("inf"), 0, 0)
 
     with nose.tools.assert_raises(AssertionError):
-        line.Line(0, float("nan"), 0)
+        Line(0, float("nan"), 0)
 
 
 def test_line_intercept():
-    line1 = line.Line(0, 1, 1)
-    line2 = line.Line(1, 0, 1)
+    line1 = Line(0, 1, 1)
+    line2 = Line(1, 0, 1)
 
-    i1 = line1.intercept(line2)
+    i1 = line1.intersect(line2)
     nose.tools.assert_equal(i1, (1, 1))
 
 
 def test_line_intersect():
-    line1 = line.Line(0, 1, 1)
+    line1 = Line(0, 1, 1)
 
-    with nose.tools.assert_raises(line.Colinear):
-        line1.intercept(line1)
+    with nose.tools.assert_raises(Colinear):
+        line1.intersect(line1)
 
-    line3 = line.Line(1, -1, 1)
-    line4 = line.Line(-1, 1, -1)
-    with nose.tools.assert_raises(line.Colinear):
-        line3.intercept(line4)
+    line3 = Line(1, -1, 1)
+    line4 = Line(-1, 1, -1)
+    with nose.tools.assert_raises(Colinear):
+        line3.intersect(line4)
 
-    with nose.tools.assert_raises(line.Colinear):
-        line4.intercept(line3)
+    with nose.tools.assert_raises(Colinear):
+        line4.intersect(line3)
 
 
 def test_line_string():
-    line1 = line.Line(1, 1, 1)
+    line1 = Line(1, 1, 1)
     assert str(line1) == "Line: 1.00x + 1.00y = 1.00"
+
+
+def test_eval():
+    line1 = Line(1, -1, 0)  # y = x
+    assert line1.x_at(5) == 5, line1.x_at(5)
+    assert line1.y_at(5) == 5, line1.y_at(5)
+
+
+def test_eq_operator():
+    line1 = Line(1, -1, 0)  # y = x
+    line2 = Line(-1, 1, 0)  # y = x
+
+    assert line1 == line2
+    assert line2 == line1
+
+    line3 = Line(5, 4, 3)
+    line4 = Line(-1, 3, 7)
+
+    assert line3 != line4
+
+
+def test_alt_constructors():
+    line1 = Line.from_point_slope(0, 0, 1)
+    line2 = Line.from_two_points(0, 0, 1, 1)
+
+    assert line1 == line2
+
+    assert Line(1, 0, 1) == Line.from_point_slope(1, 0, float("inf"))
