128c176f0bb647124b47e05771b0e55fe6951152
meawoppl <meawoppl@gmail.com>
2016-12-08 09:50:44 -0800

    WIP

--- pypenrose/interactive.py
+++ pypenrose/interactive.py
@@ -6,17 +6,16 @@ import pypenrose.space as space
 
 gl_list = space.get_1d_gridlines((0.5, 0.5), 0.0, 5)
 
-# All lines inside this list should be parallel
-for l1 in gl_list:
-    plot_line_inside_bounds(l1)
-plt.show()
+# # All lines inside this list should be parallel
+# for l1 in gl_list:
+#     plot_line_inside_bounds(l1)
+# plt.show()
 
 
 linesets = space.get_nd_grid(5, ndim=5)
 
 for lineset in linesets:
     for line in lineset:
-        print(line)
         plot_line_inside_bounds(line)
 
 plt.show()
