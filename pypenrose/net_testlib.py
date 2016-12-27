bec2d8d929c706c70f436c095f91a0ba3991ad24
meawoppl <meawoppl@gmail.com>
2016-12-27 13:29:26 -0800

    Passes parse at least

--- pypenrose/net_testlib.py
+++ pypenrose/net_testlib.py
@@ -1,8 +1,8 @@
-from pypenrose.line import get_1d_gridlines
+from pypenrose.space import get_1d_gridlines
 from pypenrose.net import gridlines_to_gridgraph
 
 
-def create_testing_net():
+def get_simple_net():
     horiz = get_1d_gridlines((1, 0), 0, 3)
     verti = get_1d_gridlines((0, 1), 0, 3)
 
